bd6642dacb0193ef17fd6c7ab086ffa0cbbbcef2
Harlan Brown <hbrown@nuxeo.com>
2014-05-13 10:28:46 -0400

    suggestion for using Great Images in NASA

--- imgs.py
+++ imgs.py
@@ -7,42 +7,44 @@ def pp(j):
 #params={'action':'query','list':'allimages','aiprop':'url','format':'json','aisort':'timestamp','aistart':'20131231000000','ailimit':'10'}
 #params={'action':'query','list':'categorymembers','cmtitle':'Category:Featured_pictures','cmlimit':'500','format':'json'}
 
-url='http://en.wikipedia.org/w/api.php'
 def getFile(url):
     fn=url.split('/')[-1]
     with open(fn, 'wb') as handle:
         response = requests.get(url, stream=True)
         if not response.ok:
-	    print 'something went wrong'
+            print 'something went wrong'
         for block in response.iter_content(1024):
             if not block:
                 break
             handle.write(block)
 
-def getPayload(title):
-    params={'action':'query','titles':title,'prop':'imageinfo','format':'json','iiprop':'url|extmetadata'}
+
+def createPayload(title,doctype):
+    url='http://en.wikipedia.org/w/api.php'
+    params={'action':'query','titles':title,'prop':'imageinfo','format':'json','iiprop':'url|size|dimensions|mime|mediatype|metadata|commonmetadata|extmetadata'}
+
     #timestamp|user|userid|comment|parsedcomment|canonicaltitle|url|size|dimensions|sha1|mime|thumbmime|mediatype|metadata|commonmetadata|extmetadata|archivename|bitdepth|uploadwarning
     r=requests.get(url,params=params)
+    pp(r.json())
     try:
         thisurl=r.json()['query']['pages']['-1']['imageinfo'][0]['url']
-	extmetadata=r.json()['query']['pages']['-1']['imageinfo'][0]['extmetadata']
-	imagedescription=extmetadata['ImageDescription']['value']
-	credit=extmetadata['Credit']['value']
-	date=extmetadata['DateTimeOriginal']['value']
-	payload={"entity-type": "document"}
-	payload['name']=title 
-	payload['type']='Picture'#'OfflinePicture'
-	payload['properties']={ 'dc:title':title, 'dc:description':'', 'dc:source':thisurl }
+        extmetadata=r.json()['query']['pages']['-1']['imageinfo'][0]['extmetadata']
+        imagedescription=extmetadata['ImageDescription']['value']
+        credit=extmetadata['Credit']['value']
+        date=extmetadata['DateTimeOriginal']['value']
+        payload={"entity-type": "document"}
+        payload['name']=title 
+        payload['type']=doctype
+        payload['properties']={ 'dc:title':title, 'dc:description':'', 'dc:source':thisurl }
     except KeyError:
         return None
     return payload
 
 
 def postDocToNuxeo(payload,url):
-    auth = HTTPBasicAuth('Administrator', 'Administrator')
-    headers = {'content-type': 'application/json'}
+    auth=HTTPBasicAuth('Administrator', 'Administrator')
+    headers={'content-type': 'application/json'}
     r=requests.post(url,data=json.dumps(payload),auth=auth,headers=headers)
-    print r.text
 
 
 def postFilesToNuxeo(lst):
@@ -50,50 +52,40 @@ def postFilesToNuxeo(lst):
     auth=HTTPBasicAuth('Administrator', 'Administrator')
     out=[]
     for idx,payload in enumerate(lst):
-	name=payload['name']
-	url=payload['properties']['dc:source']
-	filename=url.split('/')[-1]
+        name=payload['name']
+        url=payload['properties']['dc:source']
+        filename=url.split('/')[-1]
         # let's use tempfile and keep tabs on location of file
-	getFile(url)
+        getFile(url)
         headers = {'X-Batch-Id':'batch', 'X-File-Idx':idx, 'X-File-Name':filename}
-	files = {'file': open(filename, 'rb')}
+        files = {'file': open(filename, 'rb')}
         r=requests.post(uploadurl,auth=auth,headers=headers,files=files)
-	out.append({'index':idx,'payload':payload})
+        out.append({'index':idx,'payload':payload})
     return out 
-
-# "file:content": { "upload-batch":"mybatchid", "upload-fileId":"0" }
-#http://localhost:8080/nuxeo/site/automation/batch/files/batch
-#http://localhost:8080/nuxeo/site/automation/batch/drop/batch
-
 
 
 def main():
-    params={'action':'query','titles':'Albert Einstein','prop':'images','format':'json','imlimit':50}
+    url='http://en.wikipedia.org/w/api.php'
+    params={'action':'query','titles':'Albert Einstein','prop':'images','format':'json','imlimit':100}
     r=requests.get(url,params=params)
+    for page in r.json()['query']['pages']:
+        for image in r.json()['query']['pages'][page]['images']:
+            payload=createPayload(image['title'],'OfflinePicture')
+#            json.dumps(payload)
+#            if payload:
+#                postDocToNuxeo(payload,'http://localhost:8080/nuxeo/api/v1/path/default-domain/workspaces/OfflineEinstein')
 
-    lst=[]
+#    out=postFilesToNuxeo(lst)
+#    for i in out:
+#        i['payload']['properties']['file:content']={ "upload-batch":"batch", "upload-fileId":i['index'] }
+#        postDocToNuxeo(i['payload'],'http://localhost:8080/nuxeo/api/v1/path/default-domain/workspaces/Einstein')
     
-    for i in r.json()['query']['pages']:
-        for j in r.json()['query']['pages'][i]['images']:
-            title=j['title']
-            payload=getPayload(title)
-            if payload:
-		lst.append(payload)
-    out=postFilesToNuxeo(lst)
-    for i in out:
-        i['payload']['properties']['file:content']={ "upload-batch":"batch", "upload-fileId":i['index'] }
-        postDocToNuxeo(i['payload'],'http://localhost:8080/nuxeo/api/v1/path/default-domain/workspaces/Einstein')
 
 
-    
-#    for i in lst:
-#        postDocToNuxeo(json.dumps(i),'http://localhost:8080/nuxeo/api/v1/path/default-domain/workspaces/OfflineEinstein')
-#    postFilesToNuxeo(lst)
-#    postDocsToNuxeo(lst)
+#    requests.get('http://localhost:8080/nuxeo/api/v1/automation/batch/drop/batch')
 
 
-    requests.get('http://localhost:8080/nuxeo/api/v1/automation/batch/drop/batch')
-
+#####  http://data.nasa.gov/great-images-in-nasa/
 
 main()
 
